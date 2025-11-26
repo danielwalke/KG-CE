59d36846b1fadad753f8acead1230550fd4fd852
danielwalke <daniel.walke@ovgu.de>
2025-11-26 09:01:40 +0100

    fixed vite config bug

--- kgce_frontend/vite.config.js
+++ kgce_frontend/vite.config.js
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import typography from '@tailwindcss/typography';
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    require('@tailwindcss/typography'),
+    typography(),
   ],
   resolve: {
     alias: {
